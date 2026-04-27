ca7c7b21874a6debc2f9ddfd7d36edd425cec013
carlosruidias <carlos.ruidias.luyo@gmail.com>
2026-04-27 01:03:57 -0500

    feat: implement full contacto page layout

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -1,10 +1,5 @@
-import PagePlaceholder from './PagePlaceholder.jsx';
+import ContactPage from './ContactPage.jsx';
 
 export default function Contacto() {
-  return (
-    <PagePlaceholder
-      title="Contacto"
-      description="Pronto tendrás aquí nuestros canales directos de atención, formulario de contacto y ubicación para coordinar consultas comerciales y técnicas."
-    />
-  );
+  return <ContactPage />;
 }
